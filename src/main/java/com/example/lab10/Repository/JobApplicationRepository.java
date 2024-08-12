6e69b8b4129542263e8ff318b509918606c8363c
HussamMohammed7 <146628050+HussamMohammed7@users.noreply.github.com>
2024-08-12 14:03:39 +0300

    Update JobApplicationRepository.java

--- src/main/java/com/example/lab10/Repository/JobApplicationRepository.java
+++ src/main/java/com/example/lab10/Repository/JobApplicationRepository.java
@@ -3,5 +3,6 @@ package com.example.lab10.Repository;
 import com.example.lab10.Model.JobApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Integer> {
 }
